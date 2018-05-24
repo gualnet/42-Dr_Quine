d80ed0432a0d209b564c5eb90dcdc9d2e3522e72
galnet <galy@student.42.fr>
2018-05-24 21:01:59 +0200

    Colleen.s ok

--- src_asm/Colleen.s
+++ src_asm/Colleen.s
@@ -1,6 +1,5 @@
-section	.data
-s db "section .data%c%c.s db %c%s%c||%csection	.text%cglobal start%cglobal _main%cextern _printf%cX%c useless%cstart:%ccall _main%cret%c_main:%cpush rbp%cmov rbp, rsp->A%cICI LA BOUCLE%clea rdi, \
-[rel msg.s]%cmov rsi, 10%cmov rdx, 10%cmov rcx, 34%clea r8, [rel msg.s]%cmov r9, 34%ccall _printf%cadd rsp, 160 %c push *%cpop rbp%cret"
+section .data
+	s db "section .data%c%cs db %c%s%c%csection	.text%cglobal start%cglobal _main%cextern _printf%c%c useless%cstart:%ccall _main%cret%c_main:%cpush rbp%cmov rbp, rsp%cmov rcx, 34%c_lp:%cpush dword 10%ccmp rcx, 0%cdec rcx%cjg _lp%cmov dword [rsp + 8 * 30], 59%cmov dword [rsp + 8 * 5], 59%c_screen_it:%clea rdi, [rel s]%cmov rsi, 10%cmov rdx, 9%cmov rcx, 34%clea r8, [rel s]%cmov r9, 34%ccall _printf%c_the_end:%cadd rsp, 272 %c push *%cpop rbp%cret%c"
 section	.text
 global start
 global _main
@@ -12,40 +11,23 @@ ret
 _main:
 push rbp
 mov rbp, rsp
-
-push dword 10 ; 24 192
+mov rcx, 34
+_lp:
 push dword 10
-push dword 10 
-push dword 64
-push dword 10 ; 20 or 160
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10 ; ->A
-push dword 10 ; 10 or 80
-push dword 10
-push dword 10
-push dword 10
-push dword 59
-push dword 10
-push dword 10
-push dword 10
-push dword 10
-push dword 10
+cmp rcx, 0
+dec rcx
+jg _lp
+mov dword [rsp + 8 * 30], 59
+mov dword [rsp + 8 * 5], 59
 _screen_it:
 lea rdi, [rel s]
 mov rsi, 10
-mov rdx, 126
+mov rdx, 9
 mov rcx, 34
 lea r8, [rel s]
 mov r9, 34
 call _printf
 _the_end:
-add rsp, 192 ; push * 
+add rsp, 272 ; push *
 pop rbp
 ret
