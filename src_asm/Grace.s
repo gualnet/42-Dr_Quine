f280ed415b3c78c359b9027fb09eb2025ee48f45
Guillaume ALY <galy@e2r6p20.42.fr>
2018-06-05 14:12:21 +0200

    wip starting to rebuild sully

--- src_asm/Grace.s
+++ src_asm/Grace.s
@@ -1,6 +1,6 @@
 section .data
 string:
-.to_write db "section .data%cstring:%c.to_write db %c%s%c, 0%c.path db %c./Grace_kid.s%c, 0%csection	.text%cglobal start%cglobal _main%cextern _dprintf%c%c useless%c%%macro my_one 0%cmov rax, 0x2000005%clea rdi, [rel string.path]%cmov rsi, 0x0002 + 0x0200%cmov rdx, 0644o%csyscall%ccmp rax, 0%cjl _end%cmy_three%c%%endmacro%c%%macro my_two 0%ccall _main%cret%c_main:%cpush rbp%cmov rbp, rsp%cmov rcx, 60%c_lp:%cpush 10%cdec rcx%ccmp rcx, 0%cjg _lp%cmy_one%c_end:%cadd rsp, 480%cpop rbp%cret%c%%endmacro%c%%macro my_three 0%cmov dword [rsp + 0], 34%cmov dword [rsp + 2 * 8], 34%cmov dword [rsp + 3 * 8], 34%cmov dword [rsp + 9 * 8], 59%cmov rdi, rax%clea rsi, [rel string.to_write]%cmov rdx, 10%cmov rcx, 10%cmov r8, 34%clea r9, [rel string.to_write]%ccall _dprintf%c%%endmacro%cstart:%cmy_two", 0
+.to_write db "section .data%cstring:%c.to_write db %c%s%c, 0%c.path db %c./Grace_kid.s%c, 0%csection	.text%cglobal start%cglobal _main%cextern _dprintf%c%c useless%c%%macro my_one 0%cmov rax, 0x2000005%clea rdi, [rel string.path]%cmov rsi, 0x0002 + 0x0200%cmov rdx, 0644o%csyscall%ccmp rax, 0%cjl _end%cmy_three%c%%endmacro%c%%macro my_two 0%c%c%c_main:%cpush rbp%cmov rbp, rsp%cmov rcx, 60%c_lp:%cpush 10%cdec rcx%ccmp rcx, 0%cjg _lp%cmy_one%c_end:%cadd rsp, 480%cpop rbp%cret%c%%endmacro%c%%macro my_three 0%cmov dword [rsp + 0], 34%cmov dword [rsp + 2 * 8], 34%cmov dword [rsp + 3 * 8], 34%cmov dword [rsp + 9 * 8], 59%cmov rdi, rax%clea rsi, [rel string.to_write]%cmov rdx, 10%cmov rcx, 10%cmov r8, 34%clea r9, [rel string.to_write]%ccall _dprintf%c%%endmacro%cstart:%cmy_two", 0
 .path db "./Grace_kid.s", 0
 section	.text
 global start
@@ -18,8 +18,8 @@ jl _end
 my_three
 %endmacro
 %macro my_two 0
-call _main
-ret
+
+
 _main:
 push rbp
 mov rbp, rsp
